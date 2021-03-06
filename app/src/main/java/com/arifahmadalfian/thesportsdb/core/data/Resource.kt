2d8c12b07271dd50d8a5ce68f69533d02c3c7471
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-06 22:16:37 +0700

    AppExecutors

--- app/src/main/java/com/arifahmadalfian/thesportsdb/core/data/Resource.kt
+++ app/src/main/java/com/arifahmadalfian/thesportsdb/core/data/Resource.kt
@@ -1,4 +1,4 @@
-package com.arifahmadalfian.thesportsdb.core.data.source
+package com.arifahmadalfian.thesportsdb.core.data
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
