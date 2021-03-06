4c54e19186af57553371a83b972a99c9f1db9750
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-06 21:58:50 +0700

    RemoteDataSource.kt

---
+++ app/src/main/java/com/arifahmadalfian/thesportsdb/core/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,37 @@
+package com.arifahmadalfian.thesportsdb.core.data.source.remote
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.arifahmadalfian.thesportsdb.core.data.source.remote.network.ApiResponse
+import com.arifahmadalfian.thesportsdb.core.data.source.remote.network.ApiService
+import com.arifahmadalfian.thesportsdb.core.data.source.remote.response.ListSportResponse
+import com.arifahmadalfian.thesportsdb.core.data.source.remote.response.SportResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RemoteDataSource private constructor(private val apiService: ApiService) {
+
+   fun getAllSports(): LiveData<ApiResponse<List<SportResponse>>> {
+       val resultData = MutableLiveData<ApiResponse<List<SportResponse>>>()
+
+       val client = apiService.getList()
+       client.enqueue(object: Callback<ListSportResponse>{
+           override fun onResponse(
+               call: Call<ListSportResponse>,
+               response: Response<ListSportResponse>
+           ) {
+               val dataArray = response.body()?.sports
+               resultData.value = if (dataArray != null) ApiResponse.Success(dataArray) else ApiResponse.Empty
+           }
+
+           override fun onFailure(call: Call<ListSportResponse>, t: Throwable) {
+               resultData.value = ApiResponse.Error(t.message.toString())
+               Log.e("RemoteDataSource", t.message.toString())
+           }
+
+       })
+       return resultData
+   }
+}
