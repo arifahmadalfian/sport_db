b8808df4f403433f5f886d08292219518e6d5abd
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-06 16:42:53 +0700

    add starter Layout

---
+++ app/src/main/java/com/arifahmadalfian/thesportsdb/detail/DetailSportActivity.kt
@@ -0,0 +1,12 @@
+package com.arifahmadalfian.thesportsdb.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arifahmadalfian.thesportsdb.R
+
+class DetailSportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_sport)
+    }
+}
