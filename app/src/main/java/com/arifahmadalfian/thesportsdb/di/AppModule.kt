b770aa9338a331eea5b28addfab5b802bc0e50e4
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-13 09:40:16 +0700

    AppModule

---
+++ app/src/main/java/com/arifahmadalfian/thesportsdb/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.arifahmadalfian.thesportsdb.di
+
+import com.arifahmadalfian.thesportsdb.core.domain.usecase.SportInteractor
+import com.arifahmadalfian.thesportsdb.core.domain.usecase.SportUseCase
+import com.arifahmadalfian.thesportsdb.detail.DetailSportViewModel
+import com.arifahmadalfian.thesportsdb.favorite.FavoriteViewModel
+import com.arifahmadalfian.thesportsdb.home.HomeViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<SportUseCase> { SportInteractor(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { HomeViewModel(get()) }
+    viewModel { FavoriteViewModel(get()) }
+    viewModel { DetailSportViewModel(get()) }
+}
